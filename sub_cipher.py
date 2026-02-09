b45ecda970a9d599ed8b5c233ad1aa1cc82cf832
John Syvertsen <johnaylwyn@proton.me>
2026-02-09 15:14:00 -0500

    fixed shift command logic

--- sub_cipher.py
+++ sub_cipher.py
@@ -1,6 +1,4 @@
 # TODO:
-# - Add in different key generators (shift, rotation)
-# - Implement cracker functionality
 # - Implement secure key generation
 # - Allow alphabets and common words to be controlled in a
 # config file
@@ -39,17 +37,26 @@ freq_table_path = folder_path + "/freq_table.txt"
 crack_path = folder_path + "/crack_iterations.txt"
 
 
+# content: what is being written to the file
+# path: the files location
+#
+# write_file will open a file at the specified path and write
+# content to the file. Closes the file afterwards
 def write_file(content, path):
     file = open(path, "w")
     file.write(repr(content))
     file.close()
 
 
+# plain_text: a string containing the plain text from a text file
+# key: the key specified by the user
+# alphabet: the alphabet that the key will be compared to
+#
 # this function will accept a file, a key, and an alphabet.
 # The function will create a map of the original key to the
 # alphabet, then replace each instance of a letter in the
 # plain text with the key letter associated with it
-def sub_encrypt(plain_text, key, alphabet):
+def sub_key_to_alpha(plain_text, key, alphabet):
     key_map = dict(zip(alphabet, key))
     print(f"Alphabet: {alphabet}\n")
     print(f"Key: {key}\n")
@@ -62,9 +69,13 @@ def sub_encrypt(plain_text, key, alphabet):
     return joined_result
 
 
+# cipher_text: a string containing the contents of a text file
+# key: the key specified by the user
+# alphabet: the alphabet that the key will be compared to
+#
 # this function operates in the same way, but rather than mapping
 # the key to the alphabet, it maps the alphabet to the key
-def sub_decrypt(cipher_text, key, alphabet):
+def sub_alpha_to_key(cipher_text, key, alphabet):
     alpha_map = dict(zip(key, alphabet))
     print(f"Alphabet: {alphabet}\n")
     print(f"Key: {key}\n")
@@ -77,6 +88,8 @@ def sub_decrypt(cipher_text, key, alphabet):
     return joined_result
 
 
+# alphabet: the alphabet that the key will be generated from
+#
 # gen_key will take the english alphabet, break it into a list,
 # shuffle the list, join the list into a string, and return the
 # result
@@ -89,6 +102,11 @@ def gen_key(alphabet):
     return result
 
 
+# shift_count: how many letters the user wants to move
+# alphabet: the alphabet that the key will be generated from
+#
+# gen_shift_key will split the alphabet at the index shift_count
+# and swap the two halves. Returns the two halves combined
 def gen_shift_key(shift_count, alphabet):
     print("Generating shift key...")
     chars_to_shift = alphabet[:shift_count]
@@ -98,6 +116,9 @@ def gen_shift_key(shift_count, alphabet):
     return shift_key
 
 
+# text: the text that you are getting frequencies from
+# alphabet: alphabet used to find missing chars from string
+#
 # This function first generates a dictionary based on how
 # many of a letter there are, add in any letters that are
 # missing from the string, and then return a sorted list
@@ -128,24 +149,33 @@ def gen_freq_table(text, alphabet):
     return sorted_freq_table
 
 
+# text: text that you are removing special chars from
+#
 # Removes all non-alphabetical characters from a string,
 # preserves spaces
 def remove_special_chars(text):
     return re.sub(r"[^a-zA-Z\s]", "", text)
 
 
+# text: text that you are removing special chars from
+#
 # Removes all non-alphabetical characters from a string,
 # does not preserve spacing
 def remove_special_chars_sub_space(text):
     return re.sub(r"[^a-zA-Z]", "", text)
 
 
+# text: the text that you are trying to crack
+# alphabet: the alphabet that you are using as a start key
+# common_letters: letters that will be used for frequency map
+#
 # Crack will create a frequency table, then generate
 # a key based on the frequency of the letters. That
 # key will be mapped onto a dict based on the most
 # common letters. From there we will iterate through
 # the cipher text, generating a key and the text
-# based on that key.
+# based on that key. Writes to a file containing
+# all iterations
 def crack(text, alphabet, common_letters):
     print("Cracking...")
     freq_table = str(gen_freq_table(text, alphabet))
@@ -165,6 +195,7 @@ def crack(text, alphabet, common_letters):
     with open(crack_path, "w") as f:
         f.write("")
     crack_file = open(crack_path, "a")
+    crack_file.write(f"Original text: {text}\n\n")
     for key, value in freq_key_map.items():
         char_to_swap = value + key
         print(f"Chars to swap out: {char_to_swap}")
@@ -183,17 +214,18 @@ def crack(text, alphabet, common_letters):
     crack_file.close()
 
 
+# text: the text that you are trying to crack
+# alphabet: the alphabet used to generate keys
 def crack_shift(text, alphabet):
     print("Cracking using shift cipher...")
+    print(f"Original text: {text}")
     with open(crack_path, "w") as f:
         f.write("")
     crack_file = open(crack_path, "a")
+    crack_file.write(f"Original text: {text}\n\n")
     for i in range(len(alphabet)):
-        chars_to_shift = alphabet[:i]
-        shift_chars = alphabet[i:]
-        mod_key = shift_chars + chars_to_shift
+        mod_key = gen_shift_key(i, alphabet)
         key_map = dict(zip(alphabet, mod_key))
-
         mod_list = [key_map.get(letter.lower(), letter) for letter in text]
         mod_text = "".join(mod_list)
         print(f"Shift: {i}")
@@ -253,6 +285,8 @@ def main():
 
     args = parser.parse_args()
 
+    print(args)
+
     if not bool(args.encrypt) ^ bool(args.decrypt):
         print("ERR: Must select either encryption or decryption")
 
@@ -262,6 +296,7 @@ def main():
 
     elif args.encrypt:
         with open(args.textfile, "r") as text_file:
+            plain_text = text_file.read().replace("\n", "")
             os.makedirs(folder_path, exist_ok=True)
             if bool(not args.key) ^ bool(args.nokey):
                 print("ERR: Must specify key or no key")
@@ -293,22 +328,23 @@ def main():
                     )
                     return
 
-            plain_text = text_file.read().replace("\n", "")
-            cipher_output = sub_encrypt(plain_text, key, alphabet)
+            cipher_output = sub_key_to_alpha(plain_text, key, alphabet)
             write_file(cipher_output, cipher_text_path)
             write_file(key, key_path)
 
     elif args.decrypt:
         with open(args.textfile, "r") as text_file:
+            cipher_text = remove_special_chars(text_file.read()).lower()
             os.makedirs(folder_path, exist_ok=True)
             if not bool(args.key) ^ bool(args.nokey):
                 print("ERR: Must specify key or no key")
                 return
             elif args.nokey:
-                cipher_text = remove_special_chars(text_file.read()).lower()
                 if args.crack and args.shift_value:
                     crack_shift(cipher_text, alphabet)
                     return
+                elif args.shift_value:
+                    key = gen_shift_key(args.shift_value, alphabet)
                 elif args.crack:
                     crack(cipher_text, alphabet, most_common_letters)
                     return
@@ -339,8 +375,10 @@ def main():
                     )
                     return
 
-            cipher_text = remove_special_chars(text_file.read())
-            plain_output = sub_decrypt(cipher_text, key, alphabet)
+            if args.shift_value:
+                plain_output = sub_key_to_alpha(cipher_text, key, alphabet)
+            else:
+                plain_output = sub_alpha_to_key(cipher_text, key, alphabet)
             write_file(plain_output, plain_text_path)
             write_file(key, key_path)
 
